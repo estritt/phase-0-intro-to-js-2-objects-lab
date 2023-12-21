1ed0975a866c0059a65fbcdf2d909f6b8f3f106d
Genevieve Strittmatter <eastrittmatter@gmail.com>
2023-12-21 16:07:25 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name : "defaultName", 
+    streetAddress : "defaultAddress",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee, 
+        [key] : value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeWithoutKey = {...employee};
+    delete employeeWithoutKey[key];
+    return employeeWithoutKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
